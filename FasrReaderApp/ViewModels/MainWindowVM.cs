9baea64d72cfb125bffac9b4ba3578110105d69a
Penulltimo <38166546+MrRebchik@users.noreply.github.com>
2024-02-22 18:41:05 +0300

    Сделал прототип читалки

---
+++ FasrReaderApp/ViewModels/MainWindowVM.cs
@@ -0,0 +1,35 @@
+﻿using FastReaderApp.Servises;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfManagerApp1.ViewModel.Base;
+
+namespace FastReaderApp.ViewModels
+{
+    class MainWindowVM : ViewModelBase
+    {
+        TextProvider TextProvider { get; set; }
+        public string Title
+        {
+            get => "Reader";
+        }
+        private string currentWord;
+        public string CurrentWord
+        {
+            get => currentWord;
+            set => Set(ref currentWord, value);
+        }
+
+        void Increase()
+        {
+            CurrentWord = TextProvider.textArr[TextProvider.index];
+        }
+        public MainWindowVM()
+        {
+            TextProvider = new TextProvider();
+            TextProvider.IndexChanged += Increase;
+        }
+    }
+}
